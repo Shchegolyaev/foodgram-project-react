38373c3eaec46b36c8e6d31edbf00b627588c83f
Сергей Щеголяев <shchegolyaev.sergey@gmail.com>
2022-02-05 00:41:52 +0300

    add ingridient

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -4,8 +4,6 @@ from .models import (Favorite, Ingredient, IngredientInRecipe, Recipe,
                      ShoppingCart, Tag)
 
 
-
-
 class RecipeAdmin(admin.ModelAdmin):
     list_display = (
         'id', 'name', 'author', 'recipe_count',
@@ -13,8 +11,15 @@ class RecipeAdmin(admin.ModelAdmin):
     list_filter = ('name', 'author', 'tags')
 
 
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'measurement_unit',
+    )
+    list_filter = ('name',)
+
+
 admin.site.register(Recipe, RecipeAdmin)
-admin.site.register(Ingredient)
+admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Tag)
 admin.site.register(IngredientInRecipe)
 admin.site.register(Favorite)
